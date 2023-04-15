95a364d6edb21bb6715cfd0031e82235d98f37fd
Andres Diago <andresdiag@unicauca.edu.co>
2023-04-15 12:15:55 -0500

    :monocle_face: Clase 9. Otros eventos que puedes escuchar.
    Eventos scroll, keyup.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,4 +29,15 @@ export class AppComponent {
   public cumpleanhos(){
     this.person.age++;
   }
+
+  public onScroll(event: Event) {
+    const element = event.target as HTMLElement; //esto es un casting
+    console.log(element.scrollTop);
+  }
+
+  public changeName(event: Event) {
+    const element = event.target as HTMLInputElement; //esto es un casting
+    console.log(element.value);
+    this.person.name = element.value;
+  }
 }  //esto ya está conectado a la vista de app.component.html
