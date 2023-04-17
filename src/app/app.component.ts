ef522d00d3acbbfd1ad9d791692d8f89cc9112b1
Andres Diago <andresdiag@unicauca.edu.co>
2023-04-16 19:11:07 -0500

    :rocket: Clase 20. Deplay en Firebase.
    Se crea un componente recap, donde se encuentra todo lo visto en el curso, pero solo se puestra el componente del formulario.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Product } from './product.model';
 
 @Component({  //esto es un decorador de angular
   selector: 'app-root',
@@ -7,99 +6,5 @@ import { Product } from './product.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public name = 'Andres';
-  public age = 30;
-  public image = 'https://pbs.twimg.com/profile_images/953895184873467904/dRwouVDQ_400x400.jpg';
-  btnDisabled = true;
-  items = ['Andres', 'Julian', 'Felipe', 'Juan'];
-  newItem = '';
-  products: Product[] = [
-    {
-      name: 'Producto 1',
-      price: 1000,
-      image: 'https://picsum.photos/200/300',
-      description: 'All'
-    },
-    {
-      name: 'Producto 2',
-      price: 2000,
-      image: 'https://picsum.photos/200/300'
-    },
-    {
-      name: 'Producto 3',
-      price: 3000,
-      image: 'https://picsum.photos/200/300'
-    },
-    {
-      name: 'Bike',
-      price: 4000,
-      image: './assets/images/bike.jpg'
-    },
-    {
-      name: 'Album',
-      price: 5000,
-      image: './assets/images/album.jpg'
-    },
-  ];
-  color = 'red';
-  imgWidth = 200;
-  box = {
-    width: 100,
-    height: 100,
-    background: 'red'
-  }
 
-  person = {
-    name: 'Andres',
-    age: 30,
-    address: {
-      street: 'Calle falsa 123',
-      city: 'Bogota',
-      country: 'Colombia'
-    },
-    avatar: 'https://pbs.twimg.com/profile_images/953895184873467904/dRwouVDQ_400x400.jpg'
-  }
-
-  register = {
-    name: '',
-    email: '',
-    password: '',
-  }
-
-  public toggleButton() {
-    this.btnDisabled = !this.btnDisabled;
-  }
-
-  public cumpleanhos(){
-    this.person.age++;
-  }
-
-  public onScroll(event: Event) {
-    const element = event.target as HTMLElement; //esto es un casting
-    console.log(element.scrollTop);
-  }
-
-  public changeName(event: Event) {
-    const element = event.target as HTMLInputElement; //esto es un casting
-    //console.log(element.value);
-    this.person.name = element.value;
-  }
-
-  public addItem() {
-    this.items.push(this.newItem);
-    this.newItem = '';
-  }
-
-  public deleteItem(index: number) {
-    this.items.splice(index, 1); //cuantos elementos se borran a partir de la posición index
-  }
-
-  public changeColor(event: Event) {
-    const element = event.target as HTMLInputElement;
-    this.color = element.value;
-  }
-
-  public onRegister() {
-    console.log(this.register);
-  }
 }  //esto ya está conectado a la vista de app.component.html
