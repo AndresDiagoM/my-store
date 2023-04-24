1e05d0d94c84c876a68ec341414c5aaa964a8a31
Andres Diago <andresdiag@unicauca.edu.co>
2023-04-23 22:15:23 -0500

    :heavy_plus_sign: Clase 4. Uso de Outputs. Del hijo hacia el padre.
    Se importa la clase de output y eventEmitter.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/register/register.component';
 import { RecapComponent } from './components/recap/recap.component';
 import { ImgComponent } from './components/img/img.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
     RecapComponent,
-    ImgComponent
+    ImgComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-img', //con este selector es que podemos llamar a este componente en el html de otro componente
@@ -15,6 +15,20 @@ export class ImgComponent implements OnInit {
   @Input() valor: string = "valor inicial en img component";
   // @Input() es para recibir datos desde el componente padre
   @Input() img: string = "";
+  imgDefault: string = "https://www.meteorologiaenred.com/wp-content/uploads/2018/07/Planeta-J%C3%BApiter.png";
+
+  @Output() imgLoadedEvent = new EventEmitter<string>();
+
+  //--------MÉTODOS--------
+  // @Output() es para enviar datos al componente padre
+  imgError() {
+    this.img = this.imgDefault;
+  }
+
+  imgLoaded() {
+    console.log("[HIJO] imagen cargada (en hijo)");
+    this.imgLoadedEvent.emit(this.img); //emitir algo al padre
+  }
 
 }
 
