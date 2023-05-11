fd0eba0fca44936cbdb1e9cd7a24fbb547654c9f
Andres Diago <andresdiag@unicauca.edu.co>
2023-05-11 18:45:49 -0500

    :construction: Clase 6. Ciclo de vida. Hooks.
    Como ngOnInit, ngOnChanges, ngOnDestroy.

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,16 +1,43 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-img', //con este selector es que podemos llamar a este componente en el html de otro componente
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
-export class ImgComponent implements OnInit {
+export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
 
-  constructor() { }
+  constructor() {
+    //before render
+    // aqui no se debe correr nada asincrono
+    console.log("constrcutor de img component", "img: ", this.img);
+  }
+
+  ngOnChanges() { //implements OnChanges, hay que declarar el método ngOnChanges
+    // before and on the life cycle
+    // esta funcion está pendiente de los cambios de los inputs
+    console.log("ngOnChanges de img component", "img: ", this.img);
+  }
 
   ngOnInit(): void {
+    // before render
+    // aqui se puede correr algo asincrono - fecth, promesas, etc
+    console.log("ngOnInit de img component", "img: ", this.img);
   }
+
+  ngAfterViewInit() {
+    // after render
+    // aqui se maneja los hijos del componente
+    console.log("ngAfterViewInit de img component");
+  }
+
+  ngOnDestroy() {
+    // before destroy
+    // aqui se limpian los observables, intervalos, etc
+    console.log("ngOnDestroy de img component");
+  }
+
+  //--------Propiedades--------
 
   @Input() valor: string = "valor inicial en img component";
   // @Input() es para recibir datos desde el componente padre
