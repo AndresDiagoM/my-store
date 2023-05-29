4a8d275af3ee71dfec31650933003c3b83df5c3f
Andres Diago <andresdiag@unicauca.edu.co>
2023-05-29 10:28:44 -0500

    :sparkles: Clase 8. Parametros en la URL, Paginacion.
    Se añade paginacion para mostrar productos. Metodos con Firestore.

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Product, createProductDTO } from '../../models/product.model'; //importamos el modelo de datos
+import { Product, createProductDTO, Category } from '../../models/product.model'; //importamos el modelo de datos
 
 import { StoreService } from '../../services/store.service'; //importamos el servicio
 import { ProductsService } from '../../services/products.service';
@@ -31,6 +31,8 @@ export class ProductsComponent implements  OnInit {
     description: ''
   };
   createState = false;
+  limit = 10;
+  offset = 0;
 
   // -- Constructor --  //inyectamos el servicio (inyeccion de dependencias)
   constructor(
@@ -42,7 +44,7 @@ export class ProductsComponent implements  OnInit {
   }
 
   ngOnInit(): void { // Asincrono
-    this.productsService.getProducts().subscribe((productsApi) => {
+    /*this.productsService.getProductsByPage(0,10).subscribe((productsApi) => {
       //console.log(productsApi);
       // formatear los datos de la API
       for (const key in productsApi) {
@@ -54,11 +56,12 @@ export class ProductsComponent implements  OnInit {
         }
       }
       //this.products = productsApi;
-    });
-    /**this.productsService.getFirestore().subscribe((products) => {
+    });*/
+    // Con Firestore
+    this.productsService.getPageFirestore(this.offset.toString(),this.limit).subscribe((products) => {
       //console.log('products', products[0]);
       this.products = products;
-    } );*/
+    });
   }
 
   // --------- METODOS ----------
@@ -67,9 +70,11 @@ export class ProductsComponent implements  OnInit {
     this.storeService.addToCart(product);
     this.total = this.storeService.getTotal();
   }
+
   closeDetail() {
     this.detailState = !this.detailState;
   }
+
   onShowDetail(id: string) {
     if(!this.detailState) {
       this.detailState = true; // mostrar panel de detalle
@@ -86,31 +91,34 @@ export class ProductsComponent implements  OnInit {
   closeCreateProduct() {
     this.createState = !this.createState;
   }
+
   async onNewProduct(newProduct: Product | createProductDTO) {
     // Con API de firebase
-    this.productsService.create(newProduct).subscribe((response) => {
-      console.log('response', response);
-      //this.products.push(product);
-      //the server response with a object, but is not an entire product object
-      this.products.push(newProduct as Product);
-    });
+    // this.productsService.create(newProduct).subscribe((response) => {
+    //   console.log('response', response);
+    //   //this.products.push(product);
+    //   //the server response with a object, but is not an entire product object
+    //   this.products.push(newProduct as Product);
+    // });
     // Con Firestore
-    //const response= await this.productsService.createFirestore(newProduct);
-    //console.log('response', response);
+    const response= await this.productsService.createFirestore(newProduct);
+    console.log('response', response);
   }
+
   deleteProduct(product: Product) {
     // Con API de firebase
-    this.productsService.delete(product).subscribe((response) => {
-      console.log('response', response);
-      const index = this.products.indexOf(product);
-      this.products.splice(index, 1);
-      this.detailState = false; // cerrar el panel de detalle
-    });
+    // this.productsService.delete(product).subscribe((response) => {
+    //   console.log('response', response);
+    //   const index = this.products.indexOf(product);
+    //   this.products.splice(index, 1);
+    //   this.detailState = false; // cerrar el panel de detalle
+    // });
     // Con Firestore
-    /*this.productsService.deleteFirestore(product).then(() => {
+    this.productsService.deleteFirestore(product).then(() => {
       console.log('Producto eliminado');
-    });*/
+    });
   }
+
   updateProduct(product: Product) {
     // Con API de firebase
     const dto: createProductDTO = {
@@ -122,7 +130,7 @@ export class ProductsComponent implements  OnInit {
       description: product.description
     }
     //console.log('dto', product);
-    this.productsService.update(product.id, product).subscribe((response) => {
+    /*this.productsService.update(product.id, product).subscribe((response) => {
       console.log('response', response);
       this.products = this.products.map((productMap) => { // actualizar la lista de productos
         if(productMap.id === product.id) {
@@ -130,6 +138,30 @@ export class ProductsComponent implements  OnInit {
         }
         return productMap;
       });
+    });*/
+    // Con Firestore
+    this.productsService.updateFirestore(product.id, product).then(() => {
+      //console.log('Producto actualizado');
+      this.products = this.products.map((productMap) => { // actualizar la lista de productos
+        if(productMap.id === product.id) {
+          productMap = product;
+        }
+        return productMap;
+      });
+    });
+  }
+
+  // -- Metodos de paginacion --
+  nextPage() {
+    this.offset += this.limit;
+    this.productsService.getPageFirestore(this.offset.toString(), this.limit).subscribe((products) => {
+      this.products = products;
+    });
+  }
+  prevPage() {
+    this.offset -= this.limit;
+    this.productsService.getPageFirestore(this.offset.toString(), this.limit).subscribe((products) => {
+      this.products = products;
     });
   }
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Product, createProductDTO, updateProductDTO } from '../models/product.model';
-import { Firestore, collection, addDoc, collectionData, doc, deleteDoc } from '@angular/fire/firestore';
+import { Firestore, collection, addDoc, collectionData,
+        doc, deleteDoc, updateDoc,
+        limit, orderBy, query, startAfter } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -13,6 +15,7 @@ export class ProductsService {
   fakeStoreProductsUrl = 'https://fakestoreapi.com/products';
   platziProductsUrl = 'https://young-sands-07814.herokuapp.com/api/products ';
   ejemploApiFirebase1 = 'https://api-ejemplo1-24e88-default-rtdb.firebaseio.com/productos.json';
+  angularPlatziCurso3 = 'https://angular-platzi-curso3-default-rtdb.firebaseio.com/productos.json';
 
   constructor(
     private http: HttpClient,
@@ -21,27 +24,32 @@ export class ProductsService {
 
   // --------MÉTODOS--------
   getProducts() {
-    return this.http.get<Product[]>(this.ejemploApiFirebase1);
+    return this.http.get<Product[]>(this.angularPlatziCurso3);
   }
   getProduct(id: string) {
-    let ejemploApiFirebase1get = 'https://api-ejemplo1-24e88-default-rtdb.firebaseio.com/productos';
+    let ejemploApiFirebase1get = 'https://angular-platzi-curso3-default-rtdb.firebaseio.com/productos';
     return this.http.get<Product>(`${ejemploApiFirebase1get}/${id}${'.json'}`);
   }
   create(product: createProductDTO | Product) {
     // add product with a post request with incremental
-    return this.http.post<Product>(this.ejemploApiFirebase1, product);
+    return this.http.post<Product>(this.angularPlatziCurso3, product);
   }
   delete(product: Product) {
     // delete product with a delete request
-    let ejemploApi = 'https://api-ejemplo1-24e88-default-rtdb.firebaseio.com/productos';
+    let ejemploApi = 'https://angular-platzi-curso3-default-rtdb.firebaseio.com/productos';
     return this.http.delete<Product>(`${ejemploApi}/${product.id}${'.json'}`);
   }
   update(id: string, dto: updateProductDTO){
     // la url de una edicion funciona como la de un get
     // put es para reemplazar todo el objeto
     // patch es para reemplazar solo una parte del objeto
-    let ejemploApi = 'https://api-ejemplo1-24e88-default-rtdb.firebaseio.com/productos';
+    let ejemploApi = 'https://angular-platzi-curso3-default-rtdb.firebaseio.com/productos';
     return this.http.put<Product>(`${ejemploApi}/${id}${'.json'}`, dto);
+  }
+  getProductsByPage(offset: number, limit: number) {
+    return this.http.get<Product[]>(this.angularPlatziCurso3, {
+      params: {offset, limit}
+    });
   }
 
   // --------MÉTODOS FIREBASE--------
@@ -56,5 +64,17 @@ export class ProductsService {
   deleteFirestore(product: Product) {
     const productRef = doc(this.firestore, `productos/${product.id}`);
     return deleteDoc(productRef);
+  }
+  updateFirestore(id: string, dto: updateProductDTO) {
+    const productRef = doc(this.firestore, `productos/${id}`);
+    return updateDoc(productRef, dto);
+  }
+  getPageFirestore(offset: string, limitN: number) {
+    // obtener productos con la paginación usando offset y limit
+    const productRef = collection(this.firestore, 'productos');
+    //return collectionData(productRef, { idField: 'id' }) as Observable<Product[]>;
+    return collectionData(
+      query(productRef, orderBy('id'), limit(limitN), startAfter(offset))
+    ) as Observable<Product[]>;
   }
 }
